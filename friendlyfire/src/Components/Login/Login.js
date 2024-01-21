e0e576909adc59ac2398d6c305a9df4716b91602
Brandi Murray <pritchettb14@gmail.com>
2024-01-21 14:38:23 -0500

    pseudo code for login page complete

---
+++ friendlyfire/src/Components/Login/Login.js
@@ -0,0 +1,34 @@
+// this page should include......
+
+
+// ....the friendlyfire logo
+
+
+
+// ....email and password input fields
+
+
+
+
+
+
+
+
+
+
+//...The login button (will be linked to matches (i think?) page)
+
+
+
+//the divider with or in the middle
+
+
+
+
+
+// Login with google button (just css on it for now, will attach it to google later)
+
+
+
+
+// "Don't have an Accout? Sign Up " link to sign up page
