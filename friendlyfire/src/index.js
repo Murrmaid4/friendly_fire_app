1c1f0cee3b6d9002eb49461db6a05018927cd351
Brandi Murray <pritchettb14@gmail.com>
2024-01-20 22:31:34 -0500

    added initial app file structure and updated some metadata

--- friendlyfire/src/index.js
+++ friendlyfire/src/index.js
@@ -15,3 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// This page renders the pages for the user to see. Right now the app file is what's shown to users when they load this application.
