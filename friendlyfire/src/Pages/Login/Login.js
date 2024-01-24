e8b639348d577ef0c519db07d55174eb15109741
Brandi Murray <pritchettb14@gmail.com>
2024-01-23 22:51:08 -0500

    updated signup and login pages

--- friendlyfire/src/Pages/Login/Login.js
+++ friendlyfire/src/Pages/Login/Login.js
@@ -1,5 +1,68 @@
 // this page should include......
 
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Add your form submission logic here
+    // For example, you can validate the input fields, check if the passwords match, and then submit the form data
+
+    console.log('Email:', email);
+    console.log('Password:', password);
+  
+
+    // Reset the form fields
+    setEmail('');
+    setPassword('');
+   
+  };
+
+  return (
+    <div>
+ <div>{/* div for logo */}</div>
+
+        
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} required />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} required />
+      </label>
+      <br />
+      
+      <button type="submit">Login</button>
+    </form>
+    <div>
+        {/* Or divider div */}
+      </div>
+{/* this is the button that will be connecting to  */}
+      <button type="submit">Login with Google</button>
+
+    </div>
+  );
+};
+
+export default Login;
+
 
 // ....the friendlyfire logo
 
--- friendlyfire/src/Pages/Signup/Signup.js
+++ friendlyfire/src/Pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-export default function Signup() {
+
+const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -71,6 +72,8 @@ export default function Signup() {
     );
   };
 }
+
+export default Signup
 
 
 
