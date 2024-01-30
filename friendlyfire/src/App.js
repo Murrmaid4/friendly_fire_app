53ad8aa6f558f51ae7eb45aca284dd429693f603
Brandi Murray <78389456+Murrmaid4@users.noreply.github.com>
2024-01-29 22:29:33 -0500

    account page

--- friendlyfire/src/App.js
+++ friendlyfire/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import React, { useState } from "react";
 // import Signup from './pages/Signup/Signup';
-import Login from "./Pages/Login/Login";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
