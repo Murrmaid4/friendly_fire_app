9d470b1e8aead66687c12c4e2f3a3afa2d980b49
Brandi Murray <78389456+Murrmaid4@users.noreply.github.com>
2024-01-30 09:47:14 -0500

    Update Account.js

--- friendlyfire/src/Pages/Account/Account.js
+++ friendlyfire/src/Pages/Account/Account.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Logo from '../../assets/StackedLogo.svg';
+
 
 export const Account = () => {
   return (
-    <div>Account</div>
+    <div>
+        <h1>My Character Sheet</h1>
+
+        <div>{/* div for user profile picture, logo as placeholder for now */}
+        <img src={Logo} alt="friendlyfire app logo"/>
+        </div>
+
+
+
+
+
+
+
+    </div>
   )
 }
