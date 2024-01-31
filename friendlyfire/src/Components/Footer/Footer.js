69438c16f39e3e8779f217bd9209a449d2e9cf22
ca764763 <112090392+ca764763@users.noreply.github.com>
2024-01-31 13:43:23 -0500

    Finished Footer

--- friendlyfire/src/Components/Footer/Footer.js
+++ friendlyfire/src/Components/Footer/Footer.js
@@ -2,43 +2,29 @@ import {
   BrowserRouter as Router,
   Route,
   NavLink,
-  Switch,
+  Routes,
 } from "react-router-dom";
 import { FaUser, FaUsers, FaHeart } from "react-icons/fa";
 import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
 import "./Footer.css";
-import Account from "../../Pages/Account/Account.js";
-import Matches from "../../Pages/Matches/Matches.js";
-import Messages from "../../Pages/Messages/Messages.js";
 
 const Footer = () => {
   return (
+    // Needs routing
     <nav className="bottom-nav">
-      <NavLink
-        to="../../Pages/Account/Account.js"
-        exact
-        activeClassName="active"
-      >
+      <NavLink to="/profile" activeClassName="active">
         <FaUser />
         <span>Profile</span>
       </NavLink>
-      <NavLink to="/" exact activeClassName="active">
+      <NavLink to="/forums" activeClassName="active">
         <FaUsers />
         <span>Forums</span>
       </NavLink>
-      <NavLink
-        to="../../Pages/Matches/Matches.js"
-        exact
-        activeClassName="active"
-      >
+      <NavLink to="/matches" activeClassName="active">
         <FaHeart />
         <span>Match</span>
       </NavLink>
-      <NavLink
-        to="../../Pages/Messages/Messages.js"
-        exact
-        activeClassName="active"
-      >
+      <NavLink to="/messages" activeClassName="active">
         <IoChatbubbleEllipsesSharp />
         <span>Messages</span>
       </NavLink>
