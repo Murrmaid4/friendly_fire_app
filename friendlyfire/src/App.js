2da294ac50b9082f3a277aac0fcbdc9e41f70dc4
ca764763 <112090392+ca764763@users.noreply.github.com>
2024-01-31 12:18:53 -0500

    Some Footer Code

--- friendlyfire/src/App.js
+++ friendlyfire/src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import React, { useState } from "react";
-import Signup from './Pages/Signup/Signup';
-//import Login from "./Pages/Login/Login";
+// import Signup from './pages/Signup/Signup';
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Router>
-      <Signup/>
-      {/*<Login />*/}
+      {/* <Signup/> */}
+      <Login />
     </Router>
   );
 }
 
 // to test out pages you can comment out the one thats displayed currently and add ur page, will work on the linking functionality in the backend phase
-
 
 export default App;
