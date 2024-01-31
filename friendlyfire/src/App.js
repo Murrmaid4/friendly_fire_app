4eee412c7407d2f4207cdeb84f5bbb9563de52c5
ca764763 <112090392+ca764763@users.noreply.github.com>
2024-01-31 12:18:53 -0500

    Some Footer Code

--- friendlyfire/src/App.js
+++ friendlyfire/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import React, { useState } from "react";
 // import Signup from './pages/Signup/Signup';
-import Login from "./pages/Login/Login";
+import Login from "./Pages/Login/Login";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <Router>
       {/* <Signup/> */}
       <Login />
+      <Footer />
     </Router>
   );
 }
