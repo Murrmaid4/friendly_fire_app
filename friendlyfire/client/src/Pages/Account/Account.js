35ad327794c418474113064ed96400ed6ff8c400
MikaylaVen <97713262+MikaylaVen@users.noreply.github.com>
2024-04-10 12:12:37 -0400

    Account Page

--- friendlyfire/client/src/Pages/Account/Account.js
+++ friendlyfire/client/src/Pages/Account/Account.js
@@ -3,18 +3,39 @@ import Logo2 from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import SettingsIcon from '../../assets/setting.svg';
 import './Account.css';
-
-const placeholderImage1 = 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const placeholderImage2 = 'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const placeholderImage3 = 'https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+import placeholderImage1 from '../../assets/profile-pic1.png';
+import placeholderImage2 from '../../assets/profile-pic2.png';
+import placeholderImage3 from '../../assets/profile-pic3.png';
+import gameart1 from '../../assets/GameArt/stardewvalley.jpg';
+import gameart2 from '../../assets/GameArt/Valorant.jpg';
+import gameart3 from '../../assets/GameArt/fortnite.jpg';
+import gameart4 from '../../assets/GameArt/mariokart.jpg';
+import gameart5 from '../../assets/GameArt/baldursgate.jpg';
+//const placeholderImage1 = 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+//const placeholderImage2 = 'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+//const placeholderImage3 = 'https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 const images = [placeholderImage1, placeholderImage2, placeholderImage3];
-
+const gameImages = [gameart1, gameart2, gameart3, gameart4, gameart5];
+const interests = ['Animator', 'Florida International University', 'Postgrad', 'Woman', 'Agnostic', 'English', 'Long-term relationships'];
+const lifestyle = ['Socially', 'Never', 'Never', 'I want children', 'Daily'];
+const consoles = ['PC','Nintendo'];
+const gaminginterests = ['Action', 'MMORPG', 'RPG', 'Casual', 'Indie'];
 const Account = () => {
-  const aspectRatio = 4 / 3; 
+  const aspectRatio = 3 / 4;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const dragStartX = useRef(null);
-  const [width, setWidth] = useState(300); // Set the initial width
-  const [height, setHeight] = useState(width / aspectRatio); // Calculate initial height
+  const [width, setWidth] = useState(250);
+  const [height, setHeight] = useState(width / aspectRatio);
+  const [showBio, setShowBio] = useState(false);
+
+  // Define the functions with their implementations
+  const handleSwipe = (direction) => {
+    if (direction === 'left') {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    } else if (direction === 'right') {
+      setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+    }
+  };
 
   const handleMouseDown = (e) => {
     dragStartX.current = e.clientX;
@@ -37,23 +58,33 @@ const Account = () => {
     dragStartX.current = null;
   };
 
-  const handleSwipe = (direction) => {
-    if (direction === 'left' && currentImageIndex < images.length - 1) {
-      setCurrentImageIndex(currentImageIndex + 1);
-    } else if (direction === 'right' && currentImageIndex > 0) {
-      setCurrentImageIndex(currentImageIndex - 1);
+  const handleTouchStart = (e) => {
+    dragStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e) => {
+    if (dragStartX.current !== null) {
+      const deltaX = e.touches[0].clientX - dragStartX.current;
+      if (deltaX > 50) {
+        handleSwipe('left');
+        dragStartX.current = null;
+      } else if (deltaX < -50) {
+        handleSwipe('right');
+        dragStartX.current = null;
+      }
     }
+  };
+
+  const handleTouchEnd = () => {
+    dragStartX.current = null;
   };
 
   useEffect(() => {
     const handleResize = () => {
-      // Calculate height based on the aspect ratio
       const newHeight = width / aspectRatio;
-      // Set the calculated height
       setHeight(newHeight);
     };
 
-    // Add event listener for resizing
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
@@ -63,9 +94,7 @@ const Account = () => {
   return (
     <div>
       <div className="settings-icon">
-        {/* Link to the Settings page */}
         <Link to="/settings" className="settings-icon-link">
-          {/* Settings icon*/}
           <div className="settings-icon-container">
             <img
               src={SettingsIcon}
@@ -76,7 +105,6 @@ const Account = () => {
         </Link>
       </div>
 
-      {/* Logo */}
       <div className="logo-container">
         <img src={Logo2} alt="friendlyfire app logo" className="logo" style={{ width: '50px', height: '50px' }} />
         <h1>FriendlyFire</h1>
@@ -88,10 +116,11 @@ const Account = () => {
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
       >
-        {/* User profile pictures */}
         <div className="profile-pictures">
-          {/* Display current profile picture */}
           <img
             src={images[currentImageIndex]}
             alt={`Profile Picture ${currentImageIndex + 1}`}
@@ -100,17 +129,102 @@ const Account = () => {
           />
         </div>
 
-         {/* User information */}
-      <div className="profile-info">
-        <h1>Jane Doe, 21</h1>
-        <p>Not your eSports champion,  {'\n'}
-          but I've logged serious hours on Valorant.  {'\n'}
-          When not working or gaming, catch me on a  {'\n'}
-          Netflix binge or attempting to cook something  {'\n'} that looks edible.</p>
+        <div className="interests-bar">
+          <div className="interests-container">
+            {interests.map((interest, index) => (
+              <div key={index} className="interest-item">
+                {interest}
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+        <div className="profile-info">
+          <h1>Jane Doe, 24 </h1>
+          <h3> 5 Miles aways</h3>
+          {showBio ? (
+            <p>
+              Love hiking and outdoor adventures. {'\n'}
+              Avid reader and aspiring novelist. {'\n'}
+              Coffee lover and foodie at heart. {'\n'}
+              Looking for someone to share life's adventures with.
+            </p>
+          ) : (
+            <p>
+              Love hiking and outdoor adventures. {'\n'}
+              <span className="see-more" onClick={() => setShowBio(true)}>
+                See more
+              </span>
+            </p>
+          )}
+           <h1>Lifestyle</h1>
+          <div className="interests-barLifestyle">
+          <div className="interests-container">
+           
+            {lifestyle.map((lifestyle, index) => (
+              <div key={index} className="interest-item">
+                {lifestyle}
+              </div>
+            ))}
+          </div>
+        </div>
+       
+
+        <div className="gaming-highlights">
+          <h1>Gaming Highlights</h1>
+          <div className="video-container">
+            <div className="video-clip">
+              <video src="path/to/video1.mp4" controls></video>
+            </div>
+            <div className="video-clip">
+              <video src="path/to/video2.mp4" controls></video>
+            </div>
+          </div>
+
+          
+          <div className="interests-barConsoles">
+            <h1>Consoles</h1>
+          <div className="interests-containerConsoles">
+           
+            {consoles.map((consoles, index) => (
+              <div key={index} className="interest-item">
+                {consoles}
+              </div>
+            ))}
+          </div>
+
+          
+    <div className="interests-barGaming">
+      <h1>Gaming Interests</h1>
+      <div className="interests-containerGaming">
+        {gaminginterests.map((gaminginterest, index) => (
+          <div key={index} className="interest-item">
+            {gaminginterest}
+          </div>
+        ))}
       </div>
+      <div className="game-titles">
+  <h1>What I Play</h1>
+  <div>
+    {gameImages.map((image, index) => (
+      <img key={index} src={image} alt="Game Art" className="game-art" />
+    ))}
+  </div>
+</div>
+      
     </div>
   </div>
-);
-  }
+
+  <div className="actions">
+    <button className="pass">Pass</button>
+    <button className="like">Like</button>
+  </div>
+</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Account;
